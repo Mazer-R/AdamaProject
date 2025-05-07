fd1c0f6a95f34e879c146d3a24267996b1db1e90
Mazer-R <manel.cerezo1@gmail.com>
2025-05-07 23:42:03 +0200

    removed deprecated Mockbean

--- src/test/java/com/adama/backoffice/products/api/ProductControllerIntegrationTest.java
+++ src/test/java/com/adama/backoffice/products/api/ProductControllerIntegrationTest.java
@@ -12,10 +12,9 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
@@ -46,7 +45,7 @@ class ProductControllerIntegrationTest {
     private ObjectMapper objectMapper;
 
 
-    @MockBean
+    @MockitoBean
     private ProductRepository productRepository;
 
     @BeforeEach
